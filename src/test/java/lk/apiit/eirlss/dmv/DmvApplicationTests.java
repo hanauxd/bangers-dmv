885c28e94b5874d98cba3e4100f9a1ff8b914ab4
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-01 20:56:41 +0530

    Initial commit

---
+++ src/test/java/lk/apiit/eirlss/dmv/DmvApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.apiit.eirlss.dmv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
