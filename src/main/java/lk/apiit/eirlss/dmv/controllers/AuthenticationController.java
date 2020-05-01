865abf3ab3945c0679d5655af701c45946e7c3d6
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-02 05:00:36 +0530

    Authenticate user before signing in.

--- src/main/java/lk/apiit/eirlss/dmv/controllers/AuthenticationController.java
+++ src/main/java/lk/apiit/eirlss/dmv/controllers/AuthenticationController.java
@@ -1,11 +1,17 @@
 package lk.apiit.eirlss.dmv.controllers;
 
-import lk.apiit.eirlss.dmv.exceptions.CustomException;
+import lk.apiit.eirlss.dmv.dto.AuthenticationResponse;
 import lk.apiit.eirlss.dmv.models.User;
+import lk.apiit.eirlss.dmv.security.JwtUtil;
+import lk.apiit.eirlss.dmv.services.UserDetailsServiceImpl;
 import lk.apiit.eirlss.dmv.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
@@ -13,10 +19,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping
 public class AuthenticationController {
     private final UserService userService;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    @Value("${app.token.expiry-time}")
+    private String expiryTime;
 
     @Autowired
-    public AuthenticationController(UserService userService) {
+    public AuthenticationController(
+            UserService userService,
+            UserDetailsServiceImpl userDetailsService,
+            AuthenticationManager authenticationManager,
+            JwtUtil jwtUtil) {
         this.userService = userService;
+        this.userDetailsService = userDetailsService;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/register")
@@ -26,10 +45,9 @@ public class AuthenticationController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody User user) {
-        User username = userService.getUserByUsername(user.getUsername());
-        if (username == null) {
-            throw new CustomException("Username not found", HttpStatus.BAD_REQUEST);
-        }
-        return ResponseEntity.ok(username);
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
+        String jwt = jwtUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(userDetails.getUsername(), expiryTime, jwt));
     }
 }
